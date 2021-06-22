63a126d8d05925e7ec629c922de3b5258bc1635b
ilya <ilya.lebedev.98@mail.ru>
2021-06-22 18:25:05 +0300

    commit 13

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/shit.conf
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/shit
-gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:application
+sudo rm /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database qa"
+python ask/manage.py syncdb
